7deaddc0a3cecf281a6456219e289623df740f39
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-04 07:50:46 +0000

    feat: implement index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+const { isAnimating } = useCameraState()
+</script>
+
 <template>
-  <div class="flex h-[200vh] flex-col items-center justify-center">
-    <h1 class="text-4xl font-bold">Personal Site Test</h1>
-    <p class="mt-4">ホームページテストです。</p>
+  <div
+    class="pointer-events-none fixed inset-0 z-10 flex items-center justify-center transition-opacity duration-1000 ease-in-out"
+    :class="isAnimating ? 'opacity-0' : 'opacity-100'"
+  >
+    <h1 class="text-3xl text-fuchsia-950">Personal Site</h1>
   </div>
 </template>
