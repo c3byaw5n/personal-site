d9329f7eb17f2619d28edbbf011f8a1696f4cfc4
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-06 13:38:01 +0000

    refactor: useCameraState.ts

--- app/composables/useCameraState.ts
+++ app/composables/useCameraState.ts
@@ -1,7 +1,17 @@
 export const useCameraState = () => {
   const isAnimating = useState<boolean>('is-camera-animating', () => true)
 
+  const startAnimating = () => {
+    isAnimating.value = true
+  }
+
+  const stopAnimating = () => {
+    isAnimating.value = false
+  }
+
   return {
-    isAnimating,
+    isAnimating: readonly(isAnimating),
+    startAnimating,
+    stopAnimating,
   }
 }
