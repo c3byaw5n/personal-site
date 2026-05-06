f11aa4540e53fbb213a4aa37b2703324b274c647
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-06 13:41:24 +0000

    refactor: components/TheFooter.vue

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -3,7 +3,9 @@ const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <footer class="fixed bottom-0 left-0 z-50 w-full p-4 text-center text-sm text-fuchsia-900/60 tracking-wider">
+  <footer
+    class="fixed bottom-0 left-0 z-50 w-full p-4 text-center text-sm tracking-wider text-fuchsia-900/60"
+  >
     &copy; {{ currentYear }} Haruhi Arai. All rights reserved.
   </footer>
 </template>
