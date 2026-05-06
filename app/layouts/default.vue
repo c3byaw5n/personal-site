2fba65f48cebe0387680b022eadf49c152a40fd5
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-06 13:39:02 +0000

    refactor: layouts/default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="relative min-h-screen w-full">
+  <div class="relative h-screen w-full overflow-hidden">
     <div class="pointer-events-none fixed inset-0 z-0">
       <TheExperience />
     </div>
 
     <TheHeader />
 
-    <main class="relative z-10 min-h-screen w-full">
+    <main class="relative z-10">
       <slot />
     </main>
 
