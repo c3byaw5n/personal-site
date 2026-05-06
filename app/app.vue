53044d6470345661f42a3f25f9f0f0fc477fbbf6
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-06 13:37:11 +0000

    refactor: app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'ja',
+  },
+})
+
+useSeoMeta({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | 荒井 春陽` : '荒井 春陽'
+  },
+  ogType: 'website',
+  ogSiteName: '荒井 春陽',
+  twitterCard: 'summary_large_image',
+  description: '荒井 春陽のパーソナルサイト。エンジニアです。',
+  // ogImage: '',
+  // twitterImage: '',
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
