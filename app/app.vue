1ccacfa1b653f9d55e13223e88e1641235273921
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-11 13:23:05 +0900

    chore: setup useSeoMeta with og-image and site descriptions

--- app/app.vue
+++ app/app.vue
@@ -7,14 +7,20 @@ useHead({
 
 useSeoMeta({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} | 荒井 春陽` : '荒井 春陽'
+    return titleChunk ? `${titleChunk} | 春 PERSONAL SITE` : '春 PERSONAL SITE'
   },
-  ogType: 'website',
-  ogSiteName: '荒井 春陽',
-  twitterCard: 'summary_large_image',
   description: '荒井 春陽のパーソナルサイト。エンジニアです。',
-  // ogImage: '',
-  // twitterImage: '',
+
+  ogTitle: '春 PERSONAL SITE',
+  ogDescription: '荒井 春陽のパーソナルサイト。エンジニアです。',
+  ogImage: '/og-image.png',
+  ogUrl: 'https://haruhi-arai.me',
+  ogType: 'website',
+
+  twitterCard: 'summary_large_image',
+  twitterTitle: '春 PERSONAL SITE',
+  twitterDescription: '荒井 春陽のパーソナルサイト。エンジニアです。',
+  twitterImage: '/og-image.png',
 })
 </script>
 
