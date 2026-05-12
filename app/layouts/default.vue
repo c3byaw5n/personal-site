bc6c2dd7d193905f8528979d8adeab0a8767dded
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-12 14:30:15 +0000

    fix: viewport height on

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen flex-col overflow-hidden">
+  <div class="flex h-dvh flex-col overflow-hidden">
     <div class="pointer-events-none fixed inset-0 z-0">
       <TheExperience />
     </div>
