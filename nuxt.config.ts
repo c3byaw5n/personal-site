d761fc480082972615df191a9b2f72f2b333cad6
Haruhi Arai <c3byaw5n@gmail.com>
2026-05-12 17:09:50 +0900

    chore: enable proxy support in nuxt scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,8 +44,6 @@ export default defineNuxtConfig({
       registry: {
         googleAnalytics: {
           id: 'G-WG7L80WQ8K',
-          bundle: false,
-          proxy: false,
           trigger: 'onNuxtReady',
         },
       },
